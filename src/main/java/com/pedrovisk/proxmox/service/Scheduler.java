198ba145808514a8bf3dd7ba046fe8a45ade82c3
Pedro Bastos <pedro_thales@hotmail.com>
2023-08-15 18:41:06 -0300

    Include new flag to enable the shutdown or just print message

--- src/main/java/com/pedrovisk/proxmox/service/Scheduler.java
+++ src/main/java/com/pedrovisk/proxmox/service/Scheduler.java
@@ -1,5 +1,6 @@
 package com.pedrovisk.proxmox.service;
 
+import com.pedrovisk.proxmox.configuration.ProxmoxProperties;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -13,13 +14,21 @@ public class Scheduler {
     @Autowired
     ProxmoxService proxmoxService;
 
+    @Autowired
+    ProxmoxProperties proxmoxProperties;
+
     @Scheduled(cron = "${cron.expression}")
     public void shutdownJob() {
 
         if (jellyfinService.canShutdown()) {
             System.out.println("Shutting down");
-            proxmoxService.shutdownNode();
-            return;
+            if (proxmoxProperties.apiEnableShutdown()) {
+                System.out.println("Node shutdown is enabled");
+                proxmoxService.shutdownNode();
+                return;
+            }
+            System.out.println("Node shutdown is disabled");
+
         }
 
         System.out.println("Cannot shut down");
